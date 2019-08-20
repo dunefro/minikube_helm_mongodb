a473e6215c9af91dbe73b9471437093b53749906
root <root@client.lw.com>
2019-08-20 20:19:38 +0530

    first

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # minikube_helm_mongodb
+
+This script will by default install docker, minikube and helm with all of its dependencies, finally launching a pod of mongodb. The only prerequisite is to have internet connectivity and a repository of docker.
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+yum install docker-ce -y
+
+yum install socat -y
+
+systemctl enable docker --now
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+mv ./kubectl /usr/local/bin/kubectl
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+
+install minikube /usr/local/bin
+
+minikube start
+
+
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+
+kubectl --namespace kube-system create sa tiller
+
+kubectl create clusterrolebinding tiller     --clusterrole cluster-admin     --serviceaccount=kube-system:tiller
+
+helm init --service-account tiller
+
+helm repo update
+
+kubectl get deploy,svc tiller-deploy -n kube-system
+
+helm install --name my-release --set mongodbRootPassword=redhat,mongodbUsername=my-user,mongodbPassword=redhat,mongodbDatabase=mongo_db stable/mongodb
+
